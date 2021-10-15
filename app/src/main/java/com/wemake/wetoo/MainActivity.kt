70182e4a8197fad0aa3cb73f8c4230e3791a9441
smworld01 <smworld01@naver.com>
2021-10-15 14:39:59 +0900

    로그인 안할 시 로그인 액티비티 실행 추가

--- app/src/main/java/com/wemake/wetoo/MainActivity.kt
+++ app/src/main/java/com/wemake/wetoo/MainActivity.kt
@@ -3,14 +3,19 @@ package com.wemake.wetoo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wemake.wetoo.func.Auth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, LoginActivity::class.java)
+        val user = Auth(this)
 
-        startActivity(intent)
+        if (user.isNotSignIn()) {
+            val intent = Intent(this, LoginActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
