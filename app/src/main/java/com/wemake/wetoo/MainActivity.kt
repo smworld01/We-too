6901c2beaba05ba3dd95b03c8d67daef5788d460
kdm <wookdm@gmail.com>
2021-11-02 12:57:07 +0900

    save

--- app/src/main/java/com/wemake/wetoo/MainActivity.kt
+++ app/src/main/java/com/wemake/wetoo/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.wemake.wetoo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wemake.wetoo.func.Auth
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnpro : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnpro = findViewById<Button>(R.id.button)
+
+        val user = Auth(this)
+
+        if (user.isNotSignIn()) {
+            val intent = Intent(this, LoginActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        btnpro.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
